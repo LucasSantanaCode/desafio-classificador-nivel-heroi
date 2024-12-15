0af572bdb71dfb5edc168998e97d90a169b4241d
LucasSantanaCode <lucas.santana.silva@outlook.com.br>
2024-12-15 11:23:31 -0300

    Classificador utilizando Switch

---
+++ classificadorswitch.js
@@ -0,0 +1,41 @@
+// Variáveis
+let nomeHeroi = "Felipão Guerreiro"
+let xpHeroi = 1500
+let rankHeroi;
+
+// Estrutura de Decisão
+switch(xpHeroi > -1){
+    case xpHeroi >= 0 && xpHeroi <= 1000:
+    rankHeroi = "Ferro";
+    break;
+
+    case xpHeroi >= 1001 && xpHeroi <= 2000:
+    rankHeroi = "Bronze";
+    break;
+
+    case xpHeroi >= 2001 && xpHeroi <= 5000:
+    rankHeroi = "Prata";
+    break;
+
+    case xpHeroi >= 5001 && xpHeroi <= 7000:
+    rankHeroi = "Ouro";
+    break;
+
+    case xpHeroi >= 7001 && xpHeroi <= 8000:
+    rankHeroi = "Platina";
+    break;
+
+    case xpHeroi >= 9001 && xpHeroi <= 10000:
+    rankHeroi = "Imortal";
+    break;
+
+    case xpHeroi >= 10001:
+    rankHeroi = "Radiante";
+    break;
+
+    default:
+    rankHeroi = "[Invalido]";
+}
+
+// Sáidas
+console.log("O Herói de nome " + nomeHeroi + " Está no nível " + rankHeroi)
